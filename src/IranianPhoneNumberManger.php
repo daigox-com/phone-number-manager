70d396584a75509ab05cadb3558603be6bc8bcd4
daigox-com <daigox.official@gmail.com>
2025-05-09 17:17:09 +0330

    Commit changes

--- src/IranianPhoneNumberManger.php
+++ src/IranianPhoneNumberManger.php
@@ -1,142 +1,238 @@
 <?php
+/**
+ * Iranian Mobile Phone Number Utility
+ * -----------------------------------
+ * A complete helper for validating, parsing and formatting Iranian mobile
+ * phone numbers, including operator detection and random number generation.
+ *
+ * @package   daigox/phone-number-manager
+ * @author    Daigox
+ * @copyright 2025
+ * @license   MIT
+ *
+ * PHP version 8.1+
+ */
+
+declare(strict_types=1);
 
 namespace Daigox\PhoneNumberManager;
 
-class IranianPhoneNumberManger
+use InvalidArgumentException;
+
+/**
+ * Static utility class for working with Iranian mobile numbers.
+ */
+final class PhoneNumberManager
 {
-    private const HAMRAH_E_AVAL = ['0910', '0911', '0912', '0913', '0914', '0915', '0916', '0917', '0918', '0919', '0990', '0991', '0992', '0993', '0994'];
-    private const IRANCELL = ['0930', '0933', '0935', '0936', '0937', '0938', '0939', '0900', '0901', '0902', '0903', '0904', '0905', '0941'];
-    private const RIGHTEL = ['0920', '0921', '0922', '0923'];
-    private const SHATEL_MOBILE = ['099810', '099811', '099812', '099813', '099814', '099815', '099816', '099817'];
-    private const SAMANTEL = ['099999', '09999'];
-    private const TELE_KISH = ['0934'];
-    private const APTEL = ['099910', '099911', '099913', '099914'];
-    private const LOTUSTEL = ['09990'];
-    private const ARIANTEL = ['09998'];
-    private const ANARSTAN = ['0994'];
+    /** Country calling code without + */
+    public const COUNTRY_CODE = '98';
+    public const LOCAL_TRUNK  = '0';
 
-    public static function isValidIranianNumber($number): bool
+    /**
+     * Operator prefixes (leading 0 included).
+     * Updated: May 2025.
+     */
+    private const OPERATOR_PREFIXES = [
+        // همراه اول – Hamrah‑e Aval / MCI
+        'hamrah-e-aval' => [
+            '0910','0911','0912','0913','0914','0915','0916','0917','0918','0919',
+            '0990','0991','0992','0993','0994','0995','0996',
+        ],
+
+        // ایرانسل – Irancell
+        'irancell' => [
+            '0930','0933','0935','0936','0937','0938','0939',
+            '0900','0901','0902','0903','0904','0905','0941',
+        ],
+
+        // رایتل – Rightel
+        'rightel' => ['0920','0921','0922','0923'],
+
+        // شاتل‌موبایل – Shatel Mobile (Full MVNO)
+        'shatel-mobile' => [
+            '099810','099811','099812','099813','099814','099815','099816','099817',
+            '099818','099819','099820','099821',
+        ],
+
+        // سامانتل – Samantel
+        'samantel' => ['09999','099999','099996','099997','099998'],
+
+        // شبکه مستقل تلفن همراه کیش – Tele Kish (TKC)
+        'tele-kish' => ['0934'],
+
+        // تالیا – Taliya (اعتباری)
+        'taliya' => ['0932'],
+
+        // اپراتور محلی اسپادان – Spadan
+        'spadan' => ['0931'],
+
+        // آپتل –&nbsp;Aptel (MVNO)
+        'aptel' => ['099910','099911','099913'],
+
+        // آذرتل – Azartel (هواداری تراکتور)
+        'azartel' => ['099914'],
+
+        // لوتوس‌تل – LotusTel
+        'lotustel' => ['09990'],
+
+        // آریان‌تل – ArianTel
+        'ariantel' => ['09998'],
+
+        // انارستان – Anarestan (سیم‌کارت دانش‌آموزی)
+        'anarstan' => ['0994','09944','09945','09908','09932','09933'],
+    ];
+
+    /* -------------------------------------------------------------------- */
+    /*  Sanitisation                                                        */
+    /* -------------------------------------------------------------------- */
+
+    private const DIGIT_MAP = [
+        '۰' => '0','۱' => '1','۲' => '2','۳' => '3','۴' => '4','۵' => '5','۶' => '6','۷' => '7','۸' => '8','۹' => '9',
+        '٠' => '0','١' => '1','٢' => '2','٣' => '3','٤' => '4','٥' => '5','٦' => '6','٧' => '7','٨' => '8','٩' => '9',
+    ];
+
+    private function __construct() {}
+
+    public static function sanitize(string $input): string
     {
-        $number = self::sanitizePhone($number);
-        return preg_match('/^(\+98|0)?9\d{9}$/', $number);
+        return strtr(trim($input), self::DIGIT_MAP);
     }
 
-    public static function getPrefix($number): string|false
+    /* -------------------------------------------------------------------- */
+    /*  Normalisation & validation                                          */
+    /* -------------------------------------------------------------------- */
+
+    public static function normalize(string $input): string
     {
-        $number = self::sanitizePhone($number);
-        if (self::isValidIranianNumber($number)) {
-            return substr(self::normalizeNumber($number), 0, 3);
+        $digits = preg_replace('/\D+/', '', self::sanitize($input));
+
+        if (str_starts_with($digits, '0098')) {
+            $digits = substr($digits, 4);
         }
-        return false;
-    }
-
-    public static function getMiddleThreeDigits($number): string|false
-    {
-        $number = self::sanitizePhone($number);
-        if (self::isValidIranianNumber($number)) {
-            return substr(self::normalizeNumber($number), 3, 3);
+        if (str_starts_with($digits, '98')) {
+            $digits = substr($digits, 2);
         }
-        return false;
-    }
-
-    public static function getLastFourDigits($number): string|false
-    {
-        $number = self::sanitizePhone($number);
-        if (self::isValidIranianNumber($number)) {
-            return substr(self::normalizeNumber($number), 6, 4);
+        if (str_starts_with($digits, '0')) {
+            $digits = substr($digits, 1);
         }
-        return false;
-    }
 
-    public static function formatNumberInternational($number): string|false
-    {
-        $number = self::sanitizePhone($number);
-        if (self::isValidIranianNumber($number)) {
-            return '+98' . substr(self::normalizeNumber($number), 0);
+        if (!preg_match('/^9\d{9}$/', $digits)) {
+            throw new InvalidArgumentException('Invalid Iranian mobile number.');
         }
-        return false;
+        return $digits;
     }
 
-    public static function formatNumberLocal($number): string|false
+    public static function isValid(string $input): bool
     {
-        $number = self::sanitizePhone($number);
-        if (self::isValidIranianNumber($number)) {
-            return '0' . substr(self::normalizeNumber($number), 0);
+        try {
+            self::normalize($input);
+            return true;
+        } catch (InvalidArgumentException) {
+            return false;
         }
-        return false;
     }
 
-    public static function formatNumberBare($number): string|false
-    {
-        $number = self::sanitizePhone($number);
-        if (self::isValidIranianNumber($number)) {
-            return substr(self::normalizeNumber($number), 0);
-        }
-        return false;
-    }
+    /* -------------------------------------------------------------------- */
+    /*  Operator & prefix utilities                                         */
+    /* -------------------------------------------------------------------- */
 
-    private static function normalizeNumber($number): string
+    public static function getOperator(string $input): ?string
     {
-        $number = self::sanitizePhone($number);
-        $number = preg_replace('/^\+98/', '', $number);
-        $number = preg_replace('/^0/', '', $number);
-        return $number;
-    }
-
-    public static function isValidPrefix($number): bool
-    {
-        $number = self::sanitizePhone($number);
-        $validPrefixes = [
-            ...self::HAMRAH_E_AVAL,
-            ...self::IRANCELL,
-            ...self::RIGHTEL,
-            ...self::SHATEL_MOBILE,
-            ...self::SAMANTEL,
-            ...self::TELE_KISH,
-            ...self::APTEL,
-            ...self::LOTUSTEL,
-            ...self::ARIANTEL,
-            ...self::ANARSTAN,
-        ];
-        $phone = self::normalizeNumber($number);
-        $sixDigits ='0' . substr($phone, 0, 6);
-        foreach ($validPrefixes as $prefix) {
-            if (strpos($sixDigits, $prefix) === 0) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public static function getOperatorName($number): string
-    {
-        $number = self::sanitizePhone($number);
-        $phone = self::normalizeNumber($number);
-        $sixDigits = '0' . substr($phone, 0, 6);
-        $operatorPrefixes = [
-            'hamrah-e-aval' => self::HAMRAH_E_AVAL,
-            'irancell' => self::IRANCELL,
-            'rightel' => self::RIGHTEL,
-            'shatel-mobile' => self::SHATEL_MOBILE,
-            'samantel' => self::SAMANTEL,
-            'tele-kish' => self::TELE_KISH,
-            'aptel' => self::APTEL,
-            'lotustel' => self::LOTUSTEL,
-            'ariantel' => self::ARIANTEL,
-            'anarstan' => self::ANARSTAN,
-        ];
-        foreach ($operatorPrefixes as $operator => $prefixes) {
-            foreach ($prefixes as $prefixe) {
-                if (strpos($sixDigits, $prefixe) === 0) {
+        $digits = self::normalize($input);
+        foreach (self::getPrefixesOrderedDesc() as $operator => $prefixes) {
+            foreach ($prefixes as $prefix) {
+                if (str_starts_with('0' . $digits, $prefix)) {
                     return $operator;
                 }
             }
         }
-        return 'other';
+        return null;
     }
 
-    public static function sanitizePhone($number): string
+    public static function hasValidPrefix(string $input): bool
     {
-        return StringManager::convertPersianNumbersToEnglish($number);
+        return self::getOperator($input) !== null;
+    }
+
+    public static function getPrefix(string $input): ?string
+    {
+        $digits = self::normalize($input);
+        foreach (self::getPrefixesOrderedDesc() as $prefixes) {
+            foreach ($prefixes as $prefix) {
+                if (str_starts_with('0' . $digits, $prefix)) {
+                    return $prefix;
+                }
+            }
+        }
+        return null;
+    }
+
+    public static function split(string $input): array
+    {
+        $digits = self::normalize($input);
+        $prefix = self::getPrefix($digits) ?? substr('0' . $digits, 0, 4);
+        $len   = strlen(ltrim($prefix, '0'));
+        return [
+            'prefix' => $prefix,
+            'middle' => substr($digits, $len, 3),
+            'last'   => substr($digits, $len + 3),
+        ];
+    }
+
+    /* -------------------------------------------------------------------- */
+    /*  Formatting                                                          */
+    /* -------------------------------------------------------------------- */
+
+    public static function formatInternational(string $input): string
+    {
+        return '+' . self::COUNTRY_CODE . self::normalize($input);
+    }
+
+    public static function formatLocal(string $input): string
+    {
+        return self::LOCAL_TRUNK . self::normalize($input);
+    }
+
+    public static function formatBare(string $input): string
+    {
+        return self::normalize($input);
+    }
+
+    public static function formatRFC3966(string $input): string
+    {
+        $parts = self::split($input);
+        return 'tel:+' . self::COUNTRY_CODE . '-' . ltrim($parts['prefix'], '0') . '-' . $parts['middle'] . '-' . $parts['last'];
+    }
+
+    /* -------------------------------------------------------------------- */
+    /*  Random number generator                                             */
+    /* -------------------------------------------------------------------- */
+
+    public static function random(?string $operator = null): string
+    {
+        if ($operator !== null && !isset(self::OPERATOR_PREFIXES[$operator])) {
+            throw new InvalidArgumentException("Unknown operator '{$operator}'.");
+        }
+        $operator   = $operator ?? array_rand(self::OPERATOR_PREFIXES);
+        $prefix     = self::OPERATOR_PREFIXES[$operator][array_rand(self::OPERATOR_PREFIXES[$operator])];
+        $barePrefix = ltrim($prefix, '0');
+        $remaining  = 10 - strlen($barePrefix);
+        $randomPart = str_pad((string)random_int(0, (10 ** $remaining) - 1), $remaining, '0', STR_PAD_LEFT);
+        return self::LOCAL_TRUNK . $barePrefix . $randomPart;
+    }
+
+    /* -------------------------------------------------------------------- */
+
+    private static function getPrefixesOrderedDesc(): array
+    {
+        static $cache;
+        if ($cache !== null) {
+            return $cache;
+        }
+        foreach (self::OPERATOR_PREFIXES as $operator => $prefixes) {
+            usort($prefixes, static fn(string $a, string $b): int => strlen($b) <=> strlen($a));
+            $cache[$operator] = $prefixes;
+        }
+        return $cache;
     }
 }
